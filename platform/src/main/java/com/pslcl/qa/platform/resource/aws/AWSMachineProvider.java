e8ef0789cd0b6fb52831812b98ef0d8ec1fed85f
Joseph Clark <joseph.clark@us.panasonic.com>
2015-06-30 11:00:58 -0600

    Added AWS resource providers.

---
+++ platform/src/main/java/com/pslcl/qa/platform/resource/aws/AWSMachineProvider.java
@@ -0,0 +1,68 @@
+package com.pslcl.qa.platform.resource.aws;
+
+import com.pslcl.qa.platform.Hash;
+import com.pslcl.qa.platform.ResourceNotFoundException;
+import com.pslcl.qa.platform.resource.MachineProvider;
+import com.pslcl.qa.platform.resource.Resource;
+
+public class AWSMachineProvider implements MachineProvider {
+
+	@Override
+	public void updateArtifact(String component, String version,
+			String platform, String name, Hash hash) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeArtifact(String component, String version,
+			String platform, String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void invalidateArtifacts(String component, String version) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setRunnerRef(int runnerRef) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getRunnerRef() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setResource(String resourceHash, String resourceDescription) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Resource bind(String resourceHash, String resourceAttributes)
+			throws ResourceNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAvailable(String resourceHash, String resourceAttributes)
+			throws ResourceNotFoundException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void cancel() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
