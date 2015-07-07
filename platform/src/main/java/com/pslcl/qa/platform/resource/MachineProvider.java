16cf25ebe3ce8d024b2bcb6aeaf1e05921336923
clintjones <clint.jones@us.panasonic.com>
2015-07-07 13:47:52 -0600

    Docs clarify the difference between two similar bind() calls.

--- platform/src/main/java/com/pslcl/qa/platform/resource/MachineProvider.java
+++ platform/src/main/java/com/pslcl/qa/platform/resource/MachineProvider.java
@@ -9,10 +9,14 @@ public interface MachineProvider extends ResourceProvider, ArtifactConsumer {
     
 	/** 
      * Acquire a Machine.
+     * 
+     * This resource must be released once it is no longer needed.
      *
      * @param resourceHash
      * @param resourceAttributes
-     * @return Machine object which represents the Machine Resource Instance.
+     * @return MachineInstance object which represents the Machine as a ResourceInstance.
+     * @note This MachineInstance is more explicitly defined than the ResourceInstance returned by ResourceProvider.bind().
+     * @see com.pslcl.qa.platform.resource#ResourceProvider
      */
 	@Override
 	public MachineInstance bind( String resourceHash, String resourceAttributes ) throws ResourceNotFoundException;
