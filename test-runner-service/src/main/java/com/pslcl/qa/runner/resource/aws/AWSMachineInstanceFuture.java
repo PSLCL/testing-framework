dd3fee2166a4c5e7970dea2c948729033f71a3ca
Adams <chad.adams@us.panasonic.com>
2015-09-30 10:10:34 -0600

    add AWSMachineInstanceFuture

---
+++ test-runner-service/src/main/java/com/pslcl/qa/runner/resource/aws/AWSMachineInstanceFuture.java
@@ -0,0 +1,14 @@
+package com.pslcl.qa.runner.resource.aws;
+
+import java.util.concurrent.Callable;
+
+import com.pslcl.qa.runner.resource.MachineInstance;
+
+public class AWSMachineInstanceFuture implements Callable<MachineInstance>
+{
+    @Override
+    public MachineInstance call() throws Exception
+    {
+        return null;
+    }
+}
