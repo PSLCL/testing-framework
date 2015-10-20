fb86c5fa3eecc5c222a883063ae7bcf17a1780a1
Adams <chad.adams@us.panasonic.com>
2015-10-20 10:47:55 -0600

    added Runner and RunnerMBean

---
+++ dtf-core/src/main/java/com/pslcl/dtf/core/runner/Runner.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2010-2015, Panasonic Corporation.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+package com.pslcl.dtf.core.runner;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+import javax.jms.Message;
+
+import org.apache.commons.daemon.Daemon;
+
+import com.pslcl.dtf.core.runner.config.RunnerConfig;
+
+public interface Runner extends Daemon, RunnerMBean, UncaughtExceptionHandler
+{
+    public RunnerConfig getConfig();
+
+    /**
+     * 
+     * @param strRunEntryNumber String representation of the run entry number, or reNum (pk_run of this entry in table run).
+     * @param message JMS message associated with reNum, used for eventual message ack
+     * @throws Exception
+     */
+    public void submitQueueStoreNumber(String strRunEntryNumber, Message message) throws Exception;
+
+    /**
+     * Test pipeline backdoor
+     * @return a RunnerMachine object;
+     */
+    public Object getRunnerMachine();
+}
