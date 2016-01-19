38d40247ae01d43811001ce5615b074f5b2693bd
Chad Adams <chad.adams@us.panasonic.com>
2016-01-19 03:19:53 -0700

    added staf process polling code

--- platform/dtf-core/src/main/java/com/pslcl/dtf/core/runner/resource/staf/futures/RunFuture.java
+++ platform/dtf-core/src/main/java/com/pslcl/dtf/core/runner/resource/staf/futures/RunFuture.java
@@ -16,13 +16,16 @@
 package com.pslcl.dtf.core.runner.resource.staf.futures;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.pslcl.dtf.core.runner.resource.instance.RunnableProgram;
 import com.pslcl.dtf.core.runner.resource.staf.ProcessCommandData;
+import com.pslcl.dtf.core.runner.resource.staf.QueryResult;
 import com.pslcl.dtf.core.runner.resource.staf.StafSupport;
+import com.pslcl.dtf.core.util.StrH;
 import com.pslcl.dtf.core.util.TabToLevel;
 
 @SuppressWarnings("javadoc")
@@ -73,6 +76,144 @@ public class RunFuture implements Callable<RunnableProgram>
         cmdData.setWait(false);
         cmdData.setContext(context);
         cmdData.setCommand(sudo + cmdData.getFileName());
-        return StafSupport.issueProcessShellRequest(cmdData);
+        StafRunnableProgram runnableProgram = StafSupport.issueProcessShellRequest(cmdData);
+        if (start)
+            return runnableProgram;
+        waitForComplete(runnableProgram, 20, 800);
+        return runnableProgram;
+    }
+
+    private void waitForComplete(StafRunnableProgram runnableProgram, int maxRetries, int maxDelay) throws Exception
+    {
+        int count = 0;
+        int totalTime = 0;
+        do
+        {
+            ++count;
+            long delay = ((long) Math.pow(2, count) * 100L);
+            delay = Math.min(delay, maxDelay);
+            log.debug("count: " + count + " delay: " + delay + " totalwait: " + StrH.scaleMilliSeconds(totalTime));
+            try
+            {
+                QueryResult result = StafSupport.processQuery(runnableProgram);
+                if (!result.isRunning())
+                    break;
+                Thread.sleep(delay);
+                totalTime += delay;
+            } catch (InterruptedException e)
+            {
+                throw new Exception("Thread canceled", e);
+            }
+            if (count >= maxRetries - 1)
+                throw new Exception("timed out");
+        } while (true);
+    }
+
+    public static class TimeoutData
+    {
+        public final long totalTime;
+        public final int maxRetries;
+        public final int maxDelay;
+
+        public TimeoutData(int maxRetries, int maxDelay, long totalTime)
+        {
+            this.maxRetries = maxRetries;
+            this.maxDelay = maxDelay;
+            this.totalTime = totalTime;
+        }
+        
+        /**
+         * Assumes a minimum of 3 retries.
+         * <p>starting delay is 200ms, thus any sourceValue given under 1400ms will return count=3, max delay=1ms.
+         * Similar errors will be seen through out the range with totalTimes exceeding the target by a fair amount.
+         * i.e. 5 minutes will be 6.8 minutes  
+         * @param value
+         * @param targetUnit
+         * @param targetMaxDelay
+         * @param maxDelayUnit
+         * @return
+         */
+        public static TimeoutData getTimeoutData(long targetValue, TimeUnit targetUnit, int targetMaxDelay, TimeUnit targetMaxDelayUnit)
+        {
+            long targetMs = TimeUnit.MILLISECONDS.convert(targetValue, targetUnit);
+            if (targetMs < 1401)
+                return new TimeoutData(3, 1, 1400);
+            long targetMaxDelayMs = TimeUnit.MILLISECONDS.convert(targetMaxDelay, targetMaxDelayUnit);
+            long totalTime = 0;
+            int count = 0;
+            do
+            {
+                ++count;
+                long delay = ((long) Math.pow(2, count) * 100L);
+                long nextDelay = ((long) Math.pow(2, count + 1) * 100L);
+                delay = Math.min(delay, targetMaxDelayMs);
+                totalTime += delay;
+                long nextTotalTime = totalTime + nextDelay; 
+                if(nextTotalTime >= targetMs -1)
+                {
+                    if(delay < targetMaxDelayMs)
+                        targetMaxDelayMs = (int)delay;
+                }
+                if (totalTime >= targetMs - 1)
+                    return new TimeoutData(count, (int) delay, totalTime);
+            } while (true);
+        }
+
+        public String toString()
+        {
+            TabToLevel format = new TabToLevel();
+            format.ttl("\n",getClass().getSimpleName(), ":");
+            format.level.incrementAndGet();
+            format.ttl("maxRetries: ", maxRetries);
+            format.ttl("maxDelay: ", maxDelay, " (", scaleMilliSeconds(maxDelay), ")");
+            format.ttl("totalTime: ", totalTime, " (",scaleMilliSeconds(totalTime), ")");
+            return format.toString();
+        }
+        
+        public static String scaleMilliSeconds(long value)
+        {
+    /*
+                    about max               about min
+    ns:              999                            1
+    mico:         999222                         1222
+    ms:        999111222                      1111222
+    sec:     59000111222                   1000111222
+    min:   3633000111222                  60000111222
+    hr:   86400000000000                3600000111222
+    */
+            value *= 1000000;
+            if(value < 0)
+                return "0";
+            
+            if(value < 1000)
+            {// nano seconds
+                return "" + value + "ns";
+            }
+            if(value < 1000000)
+            {// micro seconds
+                return "" + (value / 1000.0) + "micos";
+            }
+            
+            if(value < 1000000000)
+            {// milli seconds
+                return "" + (value / 1000000.0) + "ms";
+            }
+            
+            if(value < 60000000000L)
+            {// seconds
+                return "" + (value / 1000000000.0) + "sec";
+            }
+            
+            if(value < 3600000000000L)
+            {// minutes
+                return "" + (value / 60000000000.0) + "min";
+            }
+            
+            if(value < 86400000000000L)
+            {// hours
+                return "" + (value / 3600000000000.0) + "hr";
+            }
+            return "" + (value / 3600000000000.0) + "hr";
+        }
     }
 }
