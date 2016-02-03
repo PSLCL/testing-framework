59e259ed1309841b23347bc81d231ddbdebb1e54
jclark <joseph.clark@us.panasonic.com>
2016-02-03 15:37:47 +0000

    Fix testrun.sh errors

--- sample-test/bin/testrun.sh
+++ sample-test/bin/testrun.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Running test script"
 sleep 2
-if [ "$1" -eq "pass" ]
+if [ "$1" = "pass" ]
 then
   exit 0
 fi
-if [ "$1" -eq "fail" ]
+if [ "$1" = "fail" ]
 then
   exit 1
 fi
