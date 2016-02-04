210c0609565437e8fea7c368dd5d5f36b58b9069
clintjones <clint.jones@us.panasonic.com>
2016-02-04 12:18:21 -0700

    Add new class StepSetOffsets

---
+++ platform/dtf-runner/src/main/java/com/pslcl/dtf/runner/template/StepSetOffsets.java
@@ -0,0 +1,32 @@
+package com.pslcl.dtf.runner.template;
+
+import java.util.List;
+
+public class StepSetOffsets {
+	private int beginSetOffset = -1;
+	private int finalSetOffset = -1; // always non-negative when begin... becomes non-negative; never less than begin...
+	
+	StepSetOffsets(String stepString, List<String> setSteps, int initialSetStepCount) {
+        int tempFinalSetOffset = initialSetStepCount;
+        int setOffset = initialSetStepCount;
+        while (true) {
+            SetStep setStep = new SetStep(setSteps.get(setOffset));
+            if (!setStep.getCommand().equals(stepString))
+                break;
+            this.beginSetOffset = initialSetStepCount;
+            this.finalSetOffset = tempFinalSetOffset;
+            if (++tempFinalSetOffset >= setSteps.size())
+                break;
+            setOffset = tempFinalSetOffset; // there is another step in this set
+        }
+	}
+	
+	int getBeginSetOffset() {
+		return this.beginSetOffset;
+	}
+	
+	int getFinalSetOffset() {
+		return this.finalSetOffset;
+	}
+
+}
