9eb6417352b158ad3980101afc21a5b9224dcc7a
clintjones <clint.jones@us.panasonic.com>
2016-02-23 09:47:02 -0700

    Progress commit, to allow a merge.

--- platform/dtf-runner/src/main/java/com/pslcl/dtf/runner/template/TemplateProvider.java
+++ platform/dtf-runner/src/main/java/com/pslcl/dtf/runner/template/TemplateProvider.java
@@ -130,44 +130,29 @@ public class TemplateProvider implements ResourceStatusListener {
     }
 	
     /**
-     * Get an instanced top level template.
-     */
-    public InstancedTemplate getInstancedTemplate(RunEntryCore reCore, DBTemplate dbTemplate, RunnerMachine runnerMachine) throws Exception {
-    	InstancedTemplate iT = getInstancedTemplate(-1, reCore, dbTemplate, runnerMachine);
-    	return iT;
-    }
-        
-    /**
-     * Get an instanced template. An instanced template has executed all its steps, and has recorded enough information along the way to be reused.
-     * 
+     * Get an instanced template. An instanced template has executed all its steps. It can be reused, unless disqualified.
      * 
      * Instance a template
      *     By instantiating a new template and running its steps for the first time, or
-     *     By reusing a template and running its some of its steps again.
-     *         On reuse, some of the steps will be executed again, but includes, binds, and start will be found in place already.
-     *         On reuse, deploys and connects are also found in place, unless new target info can be specified during reuse. 
-     *         Run steps will happen again at each reuse.
+     *     By reusing a template and not running any of its steps.
      * 
-     * @param nestedStepReference
      * @param reCore
      * @param dbTemplate
      * @param runnerMachine
      * @return
      * @throws Exception
      */
-    public InstancedTemplate getInstancedTemplate(int nestedStepReference, RunEntryCore reCore, DBTemplate dbTemplate, RunnerMachine runnerMachine) throws Exception {
-        // first check our tracking- is a matching reusable template available?
+    public InstancedTemplate getInstancedTemplate(RunEntryCore reCore, DBTemplate dbTemplate, RunnerMachine runnerMachine) throws Exception {
+        // First check our tracking- is a matching reusable template available?
+        // Note: At any one time, there can exist multiple instanced templates of the same templateID string. They differ only, or nearly only, in which test run (or parent template) had used each one.
+        //       At some future time of dtf-runner shutdown, we release every template. To distinguish them, iT's constructor assigned each iT instance a unique mark number (a Long).
         InstancedTemplate iT = availableInstancedTemplates.get(dbTemplate.hash);
-        // At any one time, there can be multiple instanced templates of the same templateID string. They differ at least in which test run (or parent template) had used each one.
-        // At some future time of dtf-runner shutdown, we release every template. To distinguish them, we assign each a Long unique marker number.
-
         if (iT != null) {
             // we will now re-use iT
             availableInstancedTemplates.remove(dbTemplate.hash);
             // Note: This is early impl with no smarts to optimize anything. At this line, they asked for the instantiated template, they get it, and now it is not available to another user.
-            iT.runSteps(); // resets internal StepsParser object and uses it to run steps
         } else {
-        	iT = InstancedTemplate.createInstancedTemplate(reCore, dbTemplate, runnerMachine); // sets internal StepsParser object and uses it to run steps; assign a Long unique marker number
+        	iT = new InstancedTemplate(reCore, dbTemplate, runnerMachine); // sets internal StepsParser object, assigns a Long unique marker number, and runs template steps
         }
         return iT;
     }
@@ -191,107 +176,3 @@ public class TemplateProvider implements ResourceStatusListener {
         }
     }
 }
-
-// old parsing technique: temporarily here as a resource for future code --------------------------------------------------------------
-    
-//    /**
-//     * 
-//     * @param iT
-//     * @param stepsParser
-//     * @param stepsReference
-//     */
-//    private void processPostBindSteps(InstancedTemplate iT, StepsParser stepsParser, int stepsReference) {
-//        // parse steps after bind
-//        while (true) {
-//            String step = stepsParser.getNextStep();
-//            if (step == null)
-//                break;
-//            // process step with this rule: step command is the first set of characters, leading up to the first space char
-//            int offset = 0;
-//            String spaceTermSubString = StepsParser.getNextSpaceTerminatedSubString(step, offset);
-//            if (spaceTermSubString != null) {
-//                offset += spaceTermSubString.length();
-//                if (++offset > step.length())
-//                    offset = -1; // done parsing
-//            
-//                switch (spaceTermSubString) {
-//                case "run":
-//                    // machineRef strArtifactName strOptions
-//                    System.out.println("TemplateProvider.getInstancedTemplate() finds run as reference " + stepsReference);
-//  
-//                    // This is what actually comes in - it is missing some spaces - this is bug 7066 under app-test-platform
-////                  run[]0emit-oal-java bin%2FOperationsTest 562A789B9E66E0051F3DF3741985A0AFF67687C26DDC0EC93E774AA8FF6ADDEE-providerMode
-//                    // is this intended?
-////                  run 0 emit-oal-java bin%2FOperationsTest 562A789B9E66E0051F3DF3741985A0AFF67687C26DDC0EC93E774AA8FF6ADDEE -providerMode
-//
-////                  From an email                    
-////                    The ordering of a command is:
-////                        1.  The program action (run, start, etc.)
-////                        2.  A program declaration (resource declaration, empty for run, since the reference is not used)
-////                        3.  A machine reference
-////                        4.  An artifact reference.
-////                        5.  A possibly empty space-separated URL-encoded set of parameters.
-//                    
-//
-//                    // this approximates what is needed
-//                    strArtifactName = null;
-//                    String strRunParams = null;
-//                    machineRef = StepsParser.getNextSpaceTerminatedSubString(step, offset);
-//                    if (machineRef != null) {
-//                        offset += machineRef.length();
-//                        if (++offset > step.length())
-//                            offset = -1; // done
-//
-//                        // gather strArtifactName
-//                        strArtifactName = StepsParser.getNextSpaceTerminatedSubString(step, offset);
-//                        if (strArtifactName != null) {
-//                            offset += strArtifactName.length();
-//                            if (++offset > step.length())
-//                                offset = -1; // done
-//                            }
-//                        }
-//                        
-//                        strRunParams = StepsParser.getNextSpaceTerminatedSubString(step, offset);
-//                        // we do not further extract from step
-////                        if (strOptions != null) {
-////                            offset += strOptions.length();
-////                            if (++offset > step.length())
-////                                offset = -1; // done
-////                        }
-//                        
-//                    if (strArtifactName != null) {
-//                        // strOptions is available; it may be null
-//                        int stepRef = Integer.parseInt(machineRef);
-//                        ResourceInfo resourceInfo = iT.getResourceInfo(stepRef);
-//                        boolean success = false;
-//                        if (resourceInfo != null) {
-//                            ResourceInstance resourceInstance = resourceInfo.getResourceInstance();
-//                            if (MachineInstance.class.isInstance(resourceInstance)) {
-//                                // run program on machineInstance
-//                                MachineInstance machineInstance = MachineInstance.class.cast(resourceInstance);
-////                              try {
-//                                    String command = strArtifactName + (strRunParams!=null ? (" " +strRunParams): ""); // command is an executable command, including arguments, to be run on the machine
-//                                    machineInstance.run(command);
-//                                    ArtifactInfo artifactInfo = new ArtifactInfo(strArtifactName, null); // null because we do not know its value
-//                                    resourceInfo.setArtifactInfo(artifactInfo);
-//                                    resourceInfo.setRunParams(strRunParams);
-//                                    
-//                                    // set iT with more gathered info and instantiations and similar
-//
-//                                    success = true;
-//                                    System.out.println("TemplateProvider.getInstancedTemplate() runs to bound resource " + stepRef + ": artifactInfo: " + strArtifactName);
-////                              }
-//                            }
-//                        }
-//                    } else {
-//                        System.out.println("TemplateProvider.getInstancedTemplate() finds run to be incompletely specified");
-//                    }
-//                    break;
-//                default:
-//                    System.out.println("TemplateProvider.getInstancedTemplate() finds something unexpected: " + spaceTermSubString + " as reference " + stepsReference);
-//                    break;
-//                } // end switch()
-//                stepsReference++;
-//            } // end if()
-//        } // end while()	
-//    }
