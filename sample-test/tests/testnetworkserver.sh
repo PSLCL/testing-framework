d2199c7ce2d12d4ff79d1bd0812dcc5ee101ecdc
Joseph Clark <joseph.clark@us.panasonic.com>
2016-06-24 09:53:43 -0600

    Fix server listen address and script line endings.

--- sample-test/tests/testnetworkserver.sh
+++ sample-test/tests/testnetworkserver.sh
@@ -2,4 +2,4 @@
 echo "Running testnetworkserver.sh"
 sleep 2
 
-echo -e "HTTP/1.1 200 OK\n\n Hello Network Test" | nc -l localhost 10001
+echo -e "HTTP/1.1 200 OK\n\n Hello Network Test" | nc -l 0.0.0.0 10001
