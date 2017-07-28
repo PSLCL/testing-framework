e50485f7b68eac5ac653cf243e7210c4832f4c5d
Adams <chad.adams@us.panasonic.com>
2017-07-28 14:37:50 -0600

    need to invoke powershell from dos

--- platform/dtf-core/src/main/java/com/pslcl/dtf/core/runner/resource/staf/futures/StafRunnableProgram.java
+++ platform/dtf-core/src/main/java/com/pslcl/dtf/core/runner/resource/staf/futures/StafRunnableProgram.java
@@ -148,12 +148,14 @@ public class StafRunnableProgram implements RunnableProgram
 
             if(commandData.isWindows())
             {
+                // powershell -NoExit -Command "& {Import-Module 'C:\Program Files (x86)\AWS Tools\PowerShell\AWSPowerShell\AWSPowerShell.psd1'; Initialize-AWSDefaultConfiguration; write-s3object -keyprefix /testId/templateId/runid/instanceid -folder C:\var\opt\pslcl\dtf\log -bucketname dtf-staf-logging}
                 // "write-s3object -keyprefix /testId/templateId/runid/instanceid -folder C:\var\opt\pslcl\dtf\log -bucketname dtf-staf-logging";
-                StringBuilder cmd = new StringBuilder("write-s3object -keyprefix ");
+                StringBuilder cmd = new StringBuilder("powershell -NoExit -Command \"& {Import-Module 'C:\\Program Files (x86)\\AWS Tools\\PowerShell\\AWSPowerShell\\AWSPowerShell.psd1'; Initialize-AWSDefaultConfiguration; write-s3object -keyprefix ");
                 cmd.append(keyprefix);
                 String logFolder = StrH.stripTrailingSeparator(commandData.getLogFolder());
                 cmd.append(" -folder ").append(logFolder);
                 cmd.append(" -bucketname ").append(commandData.getS3Bucket());
+                cmd.append("}");
 
                 //@formatter:off
                 RunFuture df = new RunFuture(
